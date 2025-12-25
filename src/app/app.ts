5da428a64580dea3d2d987801fccacc0ab6d5682
senanurtuncbilek <senanurtuncbilek@gmail.com>
2025-12-25 19:23:47 +0300

    tokenın kullanıcıdan alınması

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http'; // HttpClient eklendi
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HttpClientModule], // HttpClientModule buraya eklenmeli
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -13,28 +14,49 @@ export class AppComponent {
   selectedFile: File | null = null;
   result: any = null;
 
-  constructor(private http: HttpClient) {} // Servis enjekte edildi
+
+  //private readonly TOKEN = '1234567890987654321';
+
+  userToken: string = '';
+  
+  constructor(private http: HttpClient) {}
 
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
   }
 
-onUpload() {
-  if (!this.selectedFile) return;
-
-  const formData = new FormData();
-  formData.append('image', this.selectedFile); // Python tarafı 'image' ismini bekliyor olabilir
-
-  // DİKKAT: Port 3000'den 5000'e çektik ve yolu kısalttık
-  this.http.post('http://localhost:5000/process', formData).subscribe({
-    next: (res) => {
-      this.result = res;
-      console.log('Doğrudan Python bağlantısı başarılı!');
-    },
-    error: (err) => {
-      console.error('Bağlantı Hatası:', err);
-      alert('Python sunucusuna ulaşılamadı! Port 5000 açık mı?');
+  onUpload() {
+    if (!this.selectedFile) {
+      alert("Lütfen bir dosya seçin!");
+      return;
     }
-  });
-}
+    
+    if (!this.userToken) {
+      alert("Lütfen bir API Token girin!");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('image', this.selectedFile);
+
+    
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.userToken}`
+    });
+
+    this.http.post('http://localhost:5000/process', formData, { headers }).subscribe({
+      next: (res) => {
+        this.result = res;
+        console.log('Güvenli Python bağlantısı başarılı!');
+      },
+      error: (err) => {
+        console.error('Bağlantı Hatası:', err);
+        if (err.status === 401) {
+          alert('Yetkisiz Erişim: Token hatalı veya eksik!');
+        } else {
+          alert('Python sunucusuna ulaşılamadı! Port 5000 açık mı?');
+        }
+      }
+    });
+  }
 }
